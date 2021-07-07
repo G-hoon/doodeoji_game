ecc178f9aeaafbd3cbda5cc1b3e7595388a3e844
g-hoon <G-hoon>
2021-07-07 09:00:26 +0900

    update image file of hole because of covering mole behind hole

--- source/mole.ts
+++ source/mole.ts
@@ -1,20 +1,14 @@
 class MoleObject extends HTMLElement {
-    root: ShadowRoot;
-
-    constructor() {
-      super();
-      this.root = this.attachShadow({ mode: 'open' });
-    }
-
-    connectedCallback() {
-      this.root.innerHTML = `
-            <div class="cell">
-                <div class="hole"></div>
-                <div class="mole hidden"></div>
-                <div class="hole"></div>
-            </div>
-        `;
-    }
+  // ToDo: css code into this custom component with shadow root
+  connectedCallback() {
+    this.innerHTML = `
+      <div class="cell">
+          <div class="hole"></div>
+          <div class="mole hidden"></div>
+          <div class="hole_front"></div>
+      </div>
+    `;
+  }
 }
 
 customElements.define('mole-object', MoleObject);
